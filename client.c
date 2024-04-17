51792ebc0af65937779af9d8bd8d2484da841c35
andreadv2000 <andrea.dv0012@gmail.com>
2024-04-17 23:26:05 +0200

    TCCR configuration, termios e user input configuration

--- client.c
+++ client.c
@@ -1,3 +1,81 @@
+#include <termios.h>
+
 int main(){
+
+    //*****************Initialize integer variables*****************//
+    int uart_fd, ret, msg_len;
+    //*****************Inizialize float variables*****************//
+    float user_input = 0.0;
+    //*****************Initialize char variables*****************//
+    char msg[1024];
+    char sampling_time[1024];
+
+     /*
+      * The termios functions describe a general terminal interface that
+      * is provided to control asynchronous communications ports.(man)
+    */
+
+    struct termios settings;
+
+    /*
+     * Opens the serial device file "/dev/ttyACM0" for reading and writing.
+     * The flags used in the open() function are:
+     * O_RDWR: Open for reading and writing.
+	 * O_NDELAY: Open in non-blocking mode. This means that any read or write operation on the file will not cause the calling process to wait until the device is ready. Instead, the function call will return immediately, indicating both success and failure.
+	 * O_NOCTTY: Do not make this process the system's controlling terminal. This is useful when a program opens the port to communicate with an external device and does not want to become the system's controlling terminal.
+    */
+    uart_fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if(uart_fd < 0){
+        printf("Error opening UART device\n");
+        return -1;
+    }
     
+    ret = tcgettermios(uart_fd, &settigs);
+    if(ret != 0){
+        printf("Error getting termios settings\n");
+        return 1;
+    }
+    
+   /* The termios structure(man)
+      *      Many of the functions described here have a termios_p argument
+      *     that is a pointer to a termios structure.  This structure
+      *      contains at least the following members:
+
+      *          tcflag_t c_iflag;        input modes 
+      *          tcflag_t c_oflag;        output modes 
+      *          tcflag_t c_cflag;        control modes 
+      *          tcflag_t c_lflag;        local modes 
+      *         cc_t     c_cc[NCCS];      special characters 
+   */
+
+  /* Set up serial port 
+    * options.c_cflag sets the channel configuration flag, which contains the flags that define how the channel is used, 
+    * including the number of bits per character, the number of stop bits, speed, parity, control flow, and data flow. 
+    * In this case, we set options.c_cflag to B19200 | CS8, which means "19200 bits per second, 8 bits per character". 
+  */
+   
+   settings.c_cflag |= B19200 | CS8
+   ret = tcsetattr(uart_fd, TCSANOW, &settings);
+   if(ret != 0){
+       printf("Error settings termios settings %i %s\n", ret, strerror(errno));
+       return 1;
+   }
+
+   /* Write to the serial port */
+   
+   printf("Enter sampling time in ms:");
+   scanf("%f", &user_input);
+
+   /* Convert float to string for transmission to the serial port */
+   gcvt(user_input, 3, msg);
+   strcat(msg, "\n");
+   strcpy(sampling_time, msg);
+   
+   /* Write to the serial port */
+   msg_len = strlen(msg);
+   msg_len = write(uart_fd, msg, msg_len);
+
+   printf("Sent %d bytes to serial port\n", msg_len);
+
+   /* Operazione server */
 }
--- server.c
+++ server.c
@@ -6,6 +6,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <uart.h>
+
+//******************Setting Output Pins******************//
+
+/*
+ * WGM2 = 101 for fast PWM, 8-bit, TOP = 0xFF, update OCR2x at BOTTOM, TOV1 flag set on Top
+ * WGM3 = 101 for fast PWM, 8-bit, TOP = 0xFF, update OCR3x at BOTTOM, TOV3 flag set on Top
+ * WGM4 = 101 for fast PWM, 8-bit, TOP = 0xFF, update OCR4x at BOTTOM, TOV4 flag set on Top
+ 
+  * When a timer is configured for PWM output, it continually counts from 0 up to a maximum value.
+  * When the count matches a configured compare value,an action is triggered.
+  * This action can either be to set, clear,or toggle the output pin 
+*/
+/*
+  * CS2 = 011 for prescaler = 64 => cloack = 16MHz/64 = 250kHz
+  * CS3 = 011 for prescaler = 64 => cloack = 16MHz/64 = 250kHz
+  * CS4 = 011 for prescaler = 64 => cloack = 16MHz/64 = 250kHz
+*/
+/*
+  * COM2A = 11 set OC2A on compare match(set Output to hight level), clear OC2A at BOTTOM,non-inverting mode(hight level on compare match)
+  * COM3B = 11 set OC3B on compare match(set Output to hight level), clear OC3B at BOTTOM,non-inverting mode(hight level on compare match)
+  * COM4C = 11 set OC4C on compare match(set Output to hight level), clear OC4C at BOTTOM,non-inverting mode(hight level on compare match)
+*/
+#define TCCR2A_MASK (1<<COM2A1)|(1<<COM2A0)|(1<<WGM20) // PIN 10
+#define TCCR2B_MASK (1<<CS20)|(1<<CS21)|(1<<WGM22) // PIN 10
+#define TCCR4A_MASK (1<<COM4A0)|(1<<COM4A1)|(1<<WGM40) //PIN 6 
+#define TCCR4B_MASK (1<<CS40)|(1<<CS41)|(1<<WGM42) //PIN 6
+#define TCCR3A_MASK (1<<COM3B0)|(1<<COM3B1)|(1<<WGM30) //PIN 2
+#define TCCR3B_MASK (1<<CS30)|(1<<CS31)|(1<<WGM32) //PIN 2
+
+//********************************************************/
 int main(){
   
 }
